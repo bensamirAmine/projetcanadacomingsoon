bd7cd990735f03ad48fc3f18607a4c643ab1ce68
bensamir <bensamir97@gmail.com>
2022-03-16 15:56:30 +0100

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import ComingSoon from './screens/comingSoon';
+
+
+ReactDOM.render(
+    
+      <ComingSoon/>
+      
+    ,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
